88b6ab742706305d7d01130a60a16505206a3332
Patrick Vale <patrick.vale@linainfratech.com.br>
2022-11-10 20:09:36 -0300

    create page register

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -1,8 +1,63 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import Button from "../../Components/Button";
 import * as S from "./styles";
+import image from "../../images/primeiratela.png";
 
 const Register = () => {
-  return <S.Container></S.Container>;
+  const navigate = useNavigate();
+
+  return (
+    <S.Container>
+      <S.ColumnLeft>
+        <S.Image src={image} alt="imagem" />
+        <S.Description>
+          Utilizamos inteligência artificial para realizar a avaliação corporal
+          por meio de fotos
+        </S.Description>
+      </S.ColumnLeft>
+      <S.ColumnRight>
+        <S.Form>
+          <S.Title>Fazer Register</S.Title>
+          <input type="text" />
+          <input type="text" />
+          <S.FormColumn>
+            <input type="text" />
+            <input type="text" />
+          </S.FormColumn>
+          <S.FormColumn>
+            <input type="text" />
+            <input type="text" />
+          </S.FormColumn>
+          <S.FormColumn>
+            <input type="text" />
+            <input type="text" />
+          </S.FormColumn>
+          <S.FormColumn>
+            <input type="text" />
+            <input type="text" />
+          </S.FormColumn>
+          <input type="text" />
+          <S.FormColumn>
+            <input type="text" />
+            <input type="text" />
+          </S.FormColumn>
+          <S.FormColumn>
+            <input type="text" />
+            <input type="text" />
+          </S.FormColumn>
+
+          <S.Linker to="#">Esqueceu sua senha?</S.Linker>
+
+          <Button>Cadastrar</Button>
+          <Button outline onClick={() => navigate("/login")}>
+            Cancelar
+          </Button>
+        </S.Form>
+      </S.ColumnRight>
+    </S.Container>
+  );
 };
 
 export default Register;
--- src/Pages/Register/styles.js
+++ src/Pages/Register/styles.js
@@ -1,3 +1,73 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const ColumnLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  background-color: #ffffff;
+`;
+
+export const Image = styled.img`
+  margin-bottom: 10px;
+`;
+
+export const Description = styled.span`
+  font-weight: 700;
+  color: #424a52;
+  font-size: 27px;
+  line-height: 36px;
+  text-align: center;
+  width: 570px;
+`;
+
+export const ColumnRight = styled.div`
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex: 1;
+  background-color: #e4ebee;
+`;
+
+export const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 405px;
+`;
+
+export const Title = styled.h1`
+  font-size: 40px;
+  font-weight: 700;
+  color: #424a52;
+  margin-bottom: 64px;
+`;
+
+export const Linker = styled(Link)`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #3e7d89;
+  margin-bottom: 16px;
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+export const FormColumn = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 16px;
+  :last-child {
+    margin-right: 16px;
+  }
+`;
