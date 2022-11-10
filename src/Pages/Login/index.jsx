2bd2acc09f979eecba13836ecf9460c59d00cd99
Patrick Vale <patrick.vale@linainfratech.com.br>
2022-11-10 19:36:25 -0300

    create page login

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import * as S from "./styles";
+// import image from './'
 
 const Login = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <S.ColumnLeft>
+        <S.Image alt="imagem" />
+        <S.Description>
+          Utilizamos inteligência artificial para realizar a avaliação corporal
+          por meio de fotos
+        </S.Description>
+      </S.ColumnLeft>
+      <S.ColumnRight>
+        <S.Form>
+          <S.Title>Login</S.Title>
+          <input type="text" />
+          <input type="text" />
+          <S.Linker to="#">Esqueceu sua senha?</S.Linker>
+          <button>Entrar</button>
+          <button>Cadastrar</button>
+        </S.Form>
+      </S.ColumnRight>
+    </S.Container>
+  );
 };
 
 export default Login;
